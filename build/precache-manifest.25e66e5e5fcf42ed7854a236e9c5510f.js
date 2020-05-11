81a1cbe4dea39c6ad818c64fcc27fe30a2456391
nsardov <nsardov@gmail.com>
2020-05-12 01:25:16 +0300

    add build

---
+++ build/precache-manifest.25e66e5e5fcf42ed7854a236e9c5510f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801846b02c6669a9a87452383ec77557",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4724c6a62d5da7c74853",
+    "url": "./static/css/main.c94773eb.chunk.css"
+  },
+  {
+    "revision": "e618791fe1513b813877",
+    "url": "./static/js/2.5b4ba619.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5b4ba619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4724c6a62d5da7c74853",
+    "url": "./static/js/main.66b4d1be.chunk.js"
+  },
+  {
+    "revision": "a9177ea09f72d6208a88",
+    "url": "./static/js/runtime-main.ee81f105.js"
+  }
+]);
